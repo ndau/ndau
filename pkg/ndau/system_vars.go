7048f4b49b4610d5b6cfa03caad48c0f3c858f8f
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2018-05-11 13:57:41 +0200

    Implement code to fetch system variables from the chaos chain
    We were forced to pin a particular tmlibs version in order for things
    to compile; somewhere in the transitive dependency graph, some
    requirement isn't correctly specified. One day when we go through
    and do the Big Push to Upgrade Tendermint (hopefully _after_ they
    hit 1.0!), we'll need to remove that. Can't be helped for now, though,
    assuming we want things to compile.

--- pkg/ndau/system_vars.go
+++ pkg/ndau/system_vars.go
@@ -3,7 +3,30 @@ package ndau
 import (
 	"github.com/oneiro-ndev/ndau-chain/pkg/ndau/config"
 	"github.com/pkg/errors"
+	trpc "github.com/tendermint/tendermint/rpc/client"
+	"gitlab.ndau.tech/experiments/chaostool/pkg/tool"
 )
+
+type chaosClient struct {
+	inner trpc.ABCIClient
+}
+
+// Static type assertion that chaosClient implements the SystemStore interface
+var _ config.SystemStore = (*chaosClient)(nil)
+
+func newChaosClient(address string) chaosClient {
+	return chaosClient{
+		inner: trpc.NewHTTP(address, "/websocket"),
+	}
+}
+
+// Get implements the SystemStore interface
+func (cc chaosClient) Get(namespace, key []byte) (result []byte, err error) {
+	result, _, err = tool.GetNamespacedAt(
+		cc.inner, namespace, key, 0,
+	)
+	return
+}
 
 // System retrieves a named system variable.
 //
@@ -13,23 +36,18 @@ import (
 // variables, there needs to be an indirection layer. Because we want to
 // test our code, there needs to be a second indirect where we choose
 // whether or not to divert to a mock.
-func (app *App) System(name string) ([]byte, error) {
+func (app *App) System(name string) (val []byte, err error) {
+	var ss config.SystemStore
 	if len(app.config.UseMock) > 0 {
-		return app.systemFromMock(name)
+		ss, err = config.LoadMock(app.config.UseMock)
+		if err != nil {
+			return nil, errors.Wrap(err, "System() failed to load mock")
+		}
+	} else {
+		ss = newChaosClient(app.config.ChaosAddress)
 	}
-	return app.systemFromChaos(name)
-}
 
-func (app *App) systemFromChaos(name string) ([]byte, error) {
-	return nil, nil
-}
-
-func (app *App) systemFromMock(name string) ([]byte, error) {
-	mock, err := config.LoadMock(app.config.UseMock)
-	if err != nil {
-		return nil, errors.Wrap(err, "System() failed to load mock")
-	}
-	svi, err := config.GetSVI(mock, app.config.SystemVariableIndirect)
+	svi, err := config.GetSVI(ss, app.config.SystemVariableIndirect)
 	if err != nil {
 		return nil, errors.Wrap(err, "System() could not find SVI")
 	}
@@ -38,6 +56,6 @@ func (app *App) systemFromMock(name string) ([]byte, error) {
 		return nil, errors.New("System() could not locate desired name")
 	}
 	nsk := snsk.AsNamespacedKey()
-	val, err := config.GetNSK(mock, nsk)
+	val, err = config.GetNSK(ss, nsk)
 	return val, errors.Wrap(err, "System() could not find named indirect target")
 }
