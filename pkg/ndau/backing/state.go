2807a030d1ab0883d94e2e8ba06284103adfc0fc
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2018-06-13 16:29:54 +0200

    Broken: first draft updating for metanode v0.5
    Broken because we haven't yet updated chaosnode / chaostool
    appropriately, and we need to handle those also for this to work.

--- pkg/ndau/backing/state.go
+++ pkg/ndau/backing/state.go
@@ -1,66 +1,64 @@
 package backing
 
 import (
-	"github.com/attic-labs/noms/go/datas"
-	nt "github.com/attic-labs/noms/go/types"
-	"github.com/pkg/errors"
+	"errors"
 
-	meta "github.com/oneiro-ndev/metanode/pkg/meta.app"
+	nt "github.com/attic-labs/noms/go/types"
+	meta "github.com/oneiro-ndev/metanode/pkg/meta.app/meta.state"
 )
 
-const stateName = "State"
-
-// State is a noms struct containing all Ndau chain state.
-type State nt.Struct
+// State is primarily a set of accounts
+type State struct {
+	Accounts map[string]AccountData
+}
 
 // make sure State is a metaapp.State
 var _ meta.State = (*State)(nil)
 
-func newState(db datas.Database) nt.Struct {
-	return nt.NewStruct(stateName, map[string]nt.Value{
-		// Validators is a map of public key to power
-		validatorsKey: nt.NewMap(db),
-		// Accounts is a map of Address to AccountData
-		accountsKey: nt.NewMap(db),
-	})
-}
-
 // NewState initializes the Ndau chain's state
-func NewState(db datas.Database) State {
-	return State(newState(db))
+func NewState() State {
+	return State{Accounts: make(map[string]AccountData)}
 }
 
-// LoadState gets the application state from a dataset
-//
-// If it does not exist, a new state is automatically created
-func LoadState(db datas.Database, ds datas.Dataset) (State, datas.Dataset, error) {
-	var err error
-	head, hasHead := ds.MaybeHeadValue()
-	if !hasHead {
-		head = newState(db)
-		// commit the empty head so when we go to get things later, we don't
-		// panic due to an empty dataset
-		ds, err = db.CommitValue(ds, head)
+// MarshalNoms satisfies noms' Marshaler interface
+func (s State) MarshalNoms(vrw nt.ValueReadWriter) (nt.Value, error) {
+	nm := nt.NewMap(vrw)
+	editor := nm.Edit()
+	for k, v := range s.Accounts {
+		vval, err := v.MarshalNoms(vrw)
 		if err != nil {
-			return State{}, ds, errors.Wrap(err, "LoadState failed to commit new head")
+			return nm, err
 		}
+		editor.Set(nt.String(k), vval)
 	}
-	nsS, isS := head.(nt.Struct)
-	if !isS {
-		return NewState(db), ds, errors.New("LoadState found non-struct as ds.HeadValue")
-	}
-
-	return State(nsS), ds, nil
+	return editor.Map(), nil
 }
 
-// Load a state from a DB and DS, satisfying meta.State
-func (state *State) Load(db datas.Database, ds datas.Dataset) (datas.Dataset, error) {
-	var err error
-	*state, ds, err = LoadState(db, ds)
-	return ds, err
-}
+// UnmarshalNoms satisfies noms' Unmarshaler interface
+func (s *State) UnmarshalNoms(v nt.Value) (err error) {
+	m, isMap := v.(nt.Map)
+	if !isMap {
+		return errors.New("v is not an nt.Map")
+	}
+	nm := make(map[string]AccountData)
 
-// Commit the current state and return an updated dataset
-func (state *State) Commit(db datas.Database, ds datas.Dataset) (datas.Dataset, error) {
-	return db.CommitValue(ds, nt.Struct(*state))
+	m.IterAll(func(key, value nt.Value) {
+		if err == nil {
+			ad := AccountData{}
+			err = ad.UnmarshalNoms(value)
+			if err == nil {
+				k, keyIsString := key.(nt.String)
+				if !keyIsString {
+					err = errors.New("non-nt.String key")
+				}
+				if err == nil {
+					nm[string(k)] = ad
+				}
+			}
+		}
+	})
+	if err == nil {
+		s.Accounts = nm
+	}
+	return err
 }
