cb5aca3cfb9773f7f50d34b680c129f89b50fef2
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2018-06-20 15:30:22 +0200

    Add comment about ignored variable in empty hash

--- cmd/ndaunode/empty-hash.go
+++ cmd/ndaunode/empty-hash.go
@@ -8,6 +8,8 @@ import (
 // get the hash of an empty database
 func getEmptyHash() string {
 	// create an in-memory app
+	// the ignored variable here is associated mocked data;
+	// it's safe to ignore, because these mocks are immediately discarded
 	config, _, err := config.MakeTmpMock("")
 	check(err)
 	app, err := ndau.NewApp("mem", *config)
