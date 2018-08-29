ceace8d60b7b53a3c326166b2340b1511922f7aa
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2018-08-29 17:49:48 +0200

    Add mock tx fee script

---
+++ pkg/ndau/system_vars/transaction_fees.go
@@ -0,0 +1,7 @@
+package system_vars
+
+// TxFeeScriptName is the name of the TxFeeScript system variable
+//
+// This sytem variable store the chaincode script used to calculate transaction
+// fees.
+const TxFeeScriptName = "TransactionFeeScript"
