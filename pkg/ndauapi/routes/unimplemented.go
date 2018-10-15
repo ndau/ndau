bad3f9654015d70960982a75d8518a1be9cae0a9
Kent Quirk <kentquirk@gmail.com>
2018-10-15 12:03:19 -0400

    Unimplemented function should return 501

--- pkg/ndauapi/routes/unimplemented.go
+++ pkg/ndauapi/routes/unimplemented.go
@@ -72,156 +72,156 @@ type TxTransferAndLockRequest struct {
 
 func HandleAccount(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleAccounts(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleBlockHash(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleBlockHeight(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleBlockRange(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleNumUnconfirmedTxs(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleOrderCurrent(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleOrderHash(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleOrderHeight(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleOrderHistory(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleTransactionFetch(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleChangeValidation(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleChangeSettlement(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleClaimAccount(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleClaimNodeRewards(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleCreditEAI(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleDelegate(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleLock(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleNominateNodeReward(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleNotify(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleRegisterNode(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleReleaseFromEndowment(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleSetRewardsDest(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleStake(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleTransfer(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
 
 func HandleTransferAndLock(cf cfg.Cfg) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusInternalServerError))
+		reqres.RespondJSON(w, reqres.NewAPIError("unimplemented endpoint", http.StatusNotImplemented))
 	}
 }
