cadcb76a74d951cf7544d9549c245405a9f67d90
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2018-11-08 13:07:19 +0100

    fix system vars tests

--- pkg/ndau/system_vars_test.go
+++ pkg/ndau/system_vars_test.go
@@ -5,39 +5,22 @@ import (
 
 	"github.com/oneiro-ndev/chaos/pkg/chaos/ns"
 	"github.com/oneiro-ndev/chaos/pkg/genesisfile"
-	"github.com/oneiro-ndev/msgp-well-known-types/wkt"
 	"github.com/oneiro-ndev/ndau/pkg/ndau/cache"
-	"github.com/oneiro-ndev/ndaumath/pkg/address"
-	"github.com/oneiro-ndev/ndaumath/pkg/eai"
 	"github.com/oneiro-ndev/ndaumath/pkg/signature"
-	math "github.com/oneiro-ndev/ndaumath/pkg/types"
 	"github.com/oneiro-ndev/system_vars/pkg/svi"
 	"github.com/stretchr/testify/require"
 	abci "github.com/tendermint/tendermint/abci/types"
 )
 
 var (
-	sysvalone signature.PublicKey
-	sysvaltwo signature.PrivateKey
-	bpcvalone address.Address
-	bpcvalbar eai.RTRow
+	bpcvalone signature.PublicKey
 )
 
 func init() {
 	var err error
-	sysvalone, sysvaltwo, err = signature.Generate(signature.Ed25519, nil)
+	bpcvalone, _, err = signature.Generate(signature.Ed25519, nil)
 	if err != nil {
 		panic(err)
-	}
-
-	bpcvalone, err = address.Generate(address.KindNdau, []byte("bpc value one"))
-	if err != nil {
-		panic(err)
-	}
-
-	bpcvalbar = eai.RTRow{
-		From: math.Duration(math.Day),
-		Rate: eai.RateFromPercent(10),
 	}
 }
 
@@ -56,23 +39,16 @@ func initAppSystem(t *testing.T, height uint64) *App {
 			Namespace: ns,
 			Key:       []byte(key),
 		}
-		gfile.Set(loc, value)
-		gfile.Edit(loc, func(val *genesisfile.Value) error {
+		err = gfile.Set(loc, value)
+		require.NoError(t, err)
+		err = gfile.Edit(loc, func(val *genesisfile.Value) error {
 			val.System = &tru
 			return nil
 		})
+		require.NoError(t, err)
 	}
 
-	sets(ns.System, "one", &sysvalone)
-	sets(ns.System, "two", &sysvaltwo)
-
-	sviLoc, err := gfile.FindSVIStub()
-	require.NoError(t, err)
-	require.NotNil(t, sviLoc)
-	bpc := sviLoc.Namespace
-
-	sets(bpc, "one", &bpcvalone)
-	sets(bpc, "bar", &bpcvalbar)
+	sets(ns.System, "one", &bpcvalone)
 
 	// dump the genesisfile
 	err = gfile.Dump(*app.config.UseMock)
@@ -89,33 +65,18 @@ func initAppSystem(t *testing.T, height uint64) *App {
 	return app
 }
 
-func testSystem(t *testing.T, app *App, name, expect string) {
-	// Note: all these keys/values are presets and defined in
-	// config/make_mock.go
-	var value wkt.String
-	err := app.System(name, &value)
-	require.NoError(t, err)
-	require.Equal(t, expect, string(value))
-}
-func TestAppCanGetCurrentValueOfDeferredUpdate(t *testing.T) {
+func TestAppCanGetValue(t *testing.T) {
 	app := initAppSystem(t, 0)
 	// this fixture will switch from "bpc val one" to "system value one"
 	// at height 1000. Given that we just created this app and haven't
 	// run it, we can be confident that it is still at the first value
-	testSystem(t, app, "one", "bpc val one")
+	var value signature.PublicKey
+	err := app.System("one", &value)
+	require.NoError(t, err)
+	require.Equal(t, bpcvalone.String(), value.String())
 }
 
-func TestAppCanGetFutureValueOnceHeightIsAppropriate(t *testing.T) {
-	app := initAppSystem(t, 1000)
-	testSystem(t, app, "one", "system value one")
-}
-
-func TestAppCanGetSimpleValue(t *testing.T) {
-	app := initAppSystem(t, 0)
-	testSystem(t, app, "two", "system value two")
-}
-
-func TestAppCanGetAliasedValue(t *testing.T) {
-	app := initAppSystem(t, 0)
-	testSystem(t, app, "foo", "baz")
-}
+// there used to be more tests here, but they depended on the detailed behavior
+// of SVI maps. Becuase we are operating in a genesisfile context, and genesisfiles
+// always automatically derive SVI maps on load, we can no longer test those
+// features. We've therefore just deleted the tests which can no longer run.
