fdcfccdfec30a1801ede85e2bb519e25be9fefd3
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2018-12-18 16:38:48 +0100

    use json-compatible signable bytes

--- pkg/ndau/signable_bytes_gen.go
+++ pkg/ndau/signable_bytes_gen.go
@@ -1,253 +1,185 @@
 package ndau
 
 import (
+	"bytes"
 	"encoding/binary"
+	"encoding/json"
+	"reflect"
+	"sort"
+	"strings"
 
-	"github.com/oneiro-ndev/ndaumath/pkg/address"
+	"github.com/oneiro-ndev/metanode/pkg/meta/transaction"
 )
 
-func intbytes(i uint64) []byte {
+func intbytes(i int64) []byte {
 	ib := make([]byte, 8)
-	binary.BigEndian.PutUint64(ib, i)
+	binary.BigEndian.PutUint64(ib, uint64(i))
 	return ib
+}
+
+func bytesOf(field interface{}) []byte {
+	switch x := field.(type) {
+	case json.Number:
+		i, err := x.Int64()
+		if err != nil {
+			return nil
+		}
+		return intbytes(i)
+	case string:
+		return []byte(x)
+	case bool:
+		if x {
+			return []byte{0x01}
+		} else {
+			return []byte{0x00}
+		}
+	}
+	// for lists and maps, we have no choice but to go reflective:
+	// https://stackoverflow.com/a/38748189/504550
+	v := reflect.ValueOf(field)
+	switch v.Kind() {
+	case reflect.Slice:
+		out := make([]byte, 0)
+		for idx := 0; idx < v.Len(); idx++ {
+			out = append(out, bytesOf(v.Index(idx).Interface())...)
+		}
+		return out
+	case reflect.Map:
+		// first, get the keys as a list
+		keys := make([]reflect.Value, 0, v.Len())
+		for _, k := range v.MapKeys() {
+			if k.Kind() != reflect.String {
+				panic("json dict had non-string key: " + k.Kind().String() + ": " + k.String())
+			}
+			s := k.Interface().(string)
+			if !strings.EqualFold(s, "signature") && !strings.EqualFold(s, "signatures") {
+				keys = append(keys, k)
+			}
+		}
+		// sort the keys list for a stable iteration order
+		sort.Slice(keys, func(i, j int) bool { return keys[i].Interface().(string) < keys[j].Interface().(string) })
+
+		// for each field in the sorted set, append the bytes of that field to the output
+		out := make([]byte, 0)
+		for _, key := range keys {
+			b := bytesOf(v.MapIndex(key).Interface())
+			if b == nil {
+				return nil
+			}
+			out = append(out, b...)
+		}
+		return out
+	}
+
+	panic("unknown field type")
+}
+
+// For byte-compatible interoperability with other languages, JSON in particular,
+// we can't simply operate on the bytes and native methods of our transaction.
+// Instead, we must first convert the transaction to JSON, and then operate on
+// the types available to JSON: strings, numbers, bools, lists, and maps.
+func sbOf(tx metatx.Transactable) []byte {
+	jstext, err := json.Marshal(tx)
+	if err != nil {
+		return nil
+	}
+
+	buffer := bytes.NewBuffer(jstext)
+	jsmap := make(map[string]interface{})
+
+	jsdec := json.NewDecoder(buffer)
+	jsdec.UseNumber()
+	err = jsdec.Decode(jsmap)
+	if err != nil {
+		return nil
+	}
+
+	return bytesOf(jsmap)
 }
 
 // SignableBytes partially implements metatx.Transactable for Transfer
 func (tx *Transfer) SignableBytes() []byte {
-	blen := 0 + address.AddrLength + address.AddrLength + 8 + 8
-	bytes := make([]byte, 0, blen)
-
-	bytes = append(bytes, []byte(tx.Source.String())...)
-	bytes = append(bytes, []byte(tx.Destination.String())...)
-	bytes = append(bytes, intbytes(uint64(tx.Qty))...)
-	bytes = append(bytes, intbytes(uint64(tx.Sequence))...)
-
-	return bytes
+	return sbOf(tx)
 }
 
 // SignableBytes partially implements metatx.Transactable for ChangeValidation
 func (tx *ChangeValidation) SignableBytes() []byte {
-	blen := 0 + address.AddrLength + len(tx.ValidationScript) + 8
-	for _, changevalidationNewKeys := range tx.NewKeys {
-		blen += changevalidationNewKeys.Msgsize()
-	}
-	bytes := make([]byte, 0, blen)
-
-	bytes = append(bytes, []byte(tx.Target.String())...)
-	for _, v := range tx.NewKeys {
-		vBytes, err := v.MarshalMsg(nil)
-		if err == nil {
-			return nil
-		}
-		bytes = append(bytes, vBytes...)
-	}
-	bytes = append(bytes, []byte(tx.ValidationScript)...)
-	bytes = append(bytes, intbytes(uint64(tx.Sequence))...)
-
-	return bytes
+	return sbOf(tx)
 }
 
 // SignableBytes partially implements metatx.Transactable for ReleaseFromEndowment
 func (tx *ReleaseFromEndowment) SignableBytes() []byte {
-	blen := 0 + address.AddrLength + 8 + 8
-	bytes := make([]byte, 0, blen)
-
-	bytes = append(bytes, []byte(tx.Destination.String())...)
-	bytes = append(bytes, intbytes(uint64(tx.Qty))...)
-	bytes = append(bytes, intbytes(uint64(tx.Sequence))...)
-
-	return bytes
+	return sbOf(tx)
 }
 
 // SignableBytes partially implements metatx.Transactable for ChangeSettlementPeriod
 func (tx *ChangeSettlementPeriod) SignableBytes() []byte {
-	blen := 0 + address.AddrLength + 8 + 8
-	bytes := make([]byte, 0, blen)
-
-	bytes = append(bytes, []byte(tx.Target.String())...)
-	bytes = append(bytes, intbytes(uint64(tx.Period))...)
-	bytes = append(bytes, intbytes(uint64(tx.Sequence))...)
-
-	return bytes
+	return sbOf(tx)
 }
 
 // SignableBytes partially implements metatx.Transactable for Delegate
 func (tx *Delegate) SignableBytes() []byte {
-	blen := 0 + address.AddrLength + address.AddrLength + 8
-	bytes := make([]byte, 0, blen)
-
-	bytes = append(bytes, []byte(tx.Target.String())...)
-	bytes = append(bytes, []byte(tx.Node.String())...)
-	bytes = append(bytes, intbytes(uint64(tx.Sequence))...)
-
-	return bytes
+	return sbOf(tx)
 }
 
 // SignableBytes partially implements metatx.Transactable for CreditEAI
 func (tx *CreditEAI) SignableBytes() []byte {
-	blen := 0 + address.AddrLength + 8
-	bytes := make([]byte, 0, blen)
-
-	bytes = append(bytes, []byte(tx.Node.String())...)
-	bytes = append(bytes, intbytes(uint64(tx.Sequence))...)
-
-	return bytes
+	return sbOf(tx)
 }
 
 // SignableBytes partially implements metatx.Transactable for Lock
 func (tx *Lock) SignableBytes() []byte {
-	blen := 0 + address.AddrLength + 8 + 8
-	bytes := make([]byte, 0, blen)
-
-	bytes = append(bytes, []byte(tx.Target.String())...)
-	bytes = append(bytes, intbytes(uint64(tx.Period))...)
-	bytes = append(bytes, intbytes(uint64(tx.Sequence))...)
-
-	return bytes
+	return sbOf(tx)
 }
 
 // SignableBytes partially implements metatx.Transactable for Notify
 func (tx *Notify) SignableBytes() []byte {
-	blen := 0 + address.AddrLength + 8
-	bytes := make([]byte, 0, blen)
-
-	bytes = append(bytes, []byte(tx.Target.String())...)
-	bytes = append(bytes, intbytes(uint64(tx.Sequence))...)
-
-	return bytes
+	return sbOf(tx)
 }
 
 // SignableBytes partially implements metatx.Transactable for SetRewardsDestination
 func (tx *SetRewardsDestination) SignableBytes() []byte {
-	blen := 0 + address.AddrLength + address.AddrLength + 8
-	bytes := make([]byte, 0, blen)
-
-	bytes = append(bytes, []byte(tx.Source.String())...)
-	bytes = append(bytes, []byte(tx.Destination.String())...)
-	bytes = append(bytes, intbytes(uint64(tx.Sequence))...)
-
-	return bytes
+	return sbOf(tx)
 }
 
 // SignableBytes partially implements metatx.Transactable for ClaimAccount
 func (tx *ClaimAccount) SignableBytes() []byte {
-	blen := 0 + address.AddrLength + tx.Ownership.Msgsize() + len(tx.ValidationScript) + 8
-	for _, claimaccountValidationKeys := range tx.ValidationKeys {
-		blen += claimaccountValidationKeys.Msgsize()
-	}
-	bytes := make([]byte, 0, blen)
-
-	bytes = append(bytes, []byte(tx.Target.String())...)
-	claimaccountOwnershipBytes, err := tx.Ownership.MarshalMsg(nil)
-	if err == nil {
-		return nil
-	}
-	bytes = append(bytes, claimaccountOwnershipBytes...)
-	for _, v := range tx.ValidationKeys {
-		vBytes, err := v.MarshalMsg(nil)
-		if err == nil {
-			return nil
-		}
-		bytes = append(bytes, vBytes...)
-	}
-	bytes = append(bytes, []byte(tx.ValidationScript)...)
-	bytes = append(bytes, intbytes(uint64(tx.Sequence))...)
-
-	return bytes
+	return sbOf(tx)
 }
 
 // SignableBytes partially implements metatx.Transactable for Stake
 func (tx *Stake) SignableBytes() []byte {
-	blen := 0 + address.AddrLength + address.AddrLength + 8
-	bytes := make([]byte, 0, blen)
-
-	bytes = append(bytes, []byte(tx.Target.String())...)
-	bytes = append(bytes, []byte(tx.Node.String())...)
-	bytes = append(bytes, intbytes(uint64(tx.Sequence))...)
-
-	return bytes
+	return sbOf(tx)
 }
 
 // SignableBytes partially implements metatx.Transactable for RegisterNode
 func (tx *RegisterNode) SignableBytes() []byte {
-	blen := 0 + address.AddrLength + len(tx.DistributionScript) + len(tx.RPCAddress) + 8
-	bytes := make([]byte, 0, blen)
-
-	bytes = append(bytes, []byte(tx.Node.String())...)
-	bytes = append(bytes, []byte(tx.DistributionScript)...)
-	bytes = append(bytes, []byte(tx.RPCAddress)...)
-	bytes = append(bytes, intbytes(uint64(tx.Sequence))...)
-
-	return bytes
+	return sbOf(tx)
 }
 
 // SignableBytes partially implements metatx.Transactable for NominateNodeReward
 func (tx *NominateNodeReward) SignableBytes() []byte {
-	blen := 0 + 8 + 8
-	bytes := make([]byte, 0, blen)
-
-	bytes = append(bytes, intbytes(uint64(tx.Random))...)
-	bytes = append(bytes, intbytes(uint64(tx.Sequence))...)
-
-	return bytes
+	return sbOf(tx)
 }
 
 // SignableBytes partially implements metatx.Transactable for ClaimNodeReward
 func (tx *ClaimNodeReward) SignableBytes() []byte {
-	blen := 0 + address.AddrLength + 8
-	bytes := make([]byte, 0, blen)
-
-	bytes = append(bytes, []byte(tx.Node.String())...)
-	bytes = append(bytes, intbytes(uint64(tx.Sequence))...)
-
-	return bytes
+	return sbOf(tx)
 }
 
 // SignableBytes partially implements metatx.Transactable for TransferAndLock
 func (tx *TransferAndLock) SignableBytes() []byte {
-	blen := 0 + address.AddrLength + address.AddrLength + 8 + 8 + 8
-	bytes := make([]byte, 0, blen)
-
-	bytes = append(bytes, []byte(tx.Source.String())...)
-	bytes = append(bytes, []byte(tx.Destination.String())...)
-	bytes = append(bytes, intbytes(uint64(tx.Qty))...)
-	bytes = append(bytes, intbytes(uint64(tx.Period))...)
-	bytes = append(bytes, intbytes(uint64(tx.Sequence))...)
-
-	return bytes
+	return sbOf(tx)
 }
 
 // SignableBytes partially implements metatx.Transactable for CommandValidatorChange
 func (tx *CommandValidatorChange) SignableBytes() []byte {
-	blen := 0 + len(tx.PublicKey) + 8 + 8
-	bytes := make([]byte, 0, blen)
-
-	bytes = append(bytes, []byte(tx.PublicKey)...)
-	bytes = append(bytes, intbytes(uint64(tx.Power))...)
-	bytes = append(bytes, intbytes(uint64(tx.Sequence))...)
-
-	return bytes
+	return sbOf(tx)
 }
 
 // SignableBytes partially implements metatx.Transactable for SidechainTx
 func (tx *SidechainTx) SignableBytes() []byte {
-	blen := 0 + address.AddrLength + 1 + len(tx.SidechainSignableBytes) + 8
-	for _, sidechaintxSidechainSignatures := range tx.SidechainSignatures {
-		blen += sidechaintxSidechainSignatures.Msgsize()
-	}
-	bytes := make([]byte, 0, blen)
-
-	bytes = append(bytes, []byte(tx.Source.String())...)
-	bytes = append(bytes, []byte{tx.SidechainID}...)
-	bytes = append(bytes, []byte(tx.SidechainSignableBytes)...)
-	for _, v := range tx.SidechainSignatures {
-		vBytes, err := v.MarshalMsg(nil)
-		if err == nil {
-			return nil
-		}
-		bytes = append(bytes, vBytes...)
-	}
-	bytes = append(bytes, intbytes(uint64(tx.Sequence))...)
-
-	return bytes
+	return sbOf(tx)
 }
+
+
