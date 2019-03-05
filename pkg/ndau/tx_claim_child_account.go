b8a9c49719d383f3d28b5e8e14a920d8c5d77092
Eric Schmidt <eric.schmidt@oneiro.io>
2019-03-05 15:37:44 -0800

    Stub out tx_claim_child_account.go

---
+++ pkg/ndau/tx_claim_child_account.go
@@ -0,0 +1,39 @@
+package ndau
+
+import (
+	"github.com/oneiro-ndev/ndaumath/pkg/address"
+	"github.com/oneiro-ndev/ndaumath/pkg/signature"
+	"github.com/pkg/errors"
+)
+
+// GetAccountAddresses returns the account addresses associated with this transaction type.
+func (tx *ClaimChildAccount) GetAccountAddresses() []string {
+	return []string{tx.Target.String(), tx.Child.String()}
+}
+
+// Validate returns nil if tx is valid, or an error
+func (tx *ClaimChildAccount) Validate(appI interface{}) error {
+	// TODO: Implement.
+	return nil
+}
+
+// Apply applies this tx if no error occurs
+func (tx *ClaimChildAccount) Apply(appI interface{}) error {
+	// TODO: Implement.
+	return nil
+}
+
+// GetSource implements sourcer
+func (tx *ClaimChildAccount) GetSource(*App) (address.Address, error) {
+	return tx.Target, nil
+}
+
+// GetSequence implements sequencer
+func (tx *ClaimChildAccount) GetSequence() uint64 {
+	return tx.Sequence
+}
+
+// GetSignatures implements signeder
+func (tx *ClaimChildAccount) GetSignatures() []signature.Signature {
+	return tx.Signatures
+}
