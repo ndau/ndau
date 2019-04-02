2d2dff81f98ee0dc30eb791d148ceb1819354e55
Eric Schmidt <eric.schmidt@oneiro.io>
2019-04-02 09:45:29 -0700

    Implement /system/history endpoint

--- pkg/ndauapi/routes/sysvars.go
+++ pkg/ndauapi/routes/sysvars.go
@@ -1,8 +1,10 @@
 package routes
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/go-zoo/bone"
 	"github.com/oneiro-ndev/ndau/pkg/ndauapi/cfg"
 	"github.com/oneiro-ndev/ndau/pkg/ndauapi/reqres"
 	"github.com/oneiro-ndev/ndau/pkg/ndauapi/ws"
@@ -29,5 +31,36 @@ func HandleSystemAll(cf cfg.Cfg) http.HandlerFunc {
 			return
 		}
 		reqres.RespondJSON(w, reqres.OKResponse(values))
+	}
+}
+
+// HandleSystemHistory returns the history of a given system variable.
+func HandleSystemHistory(cf cfg.Cfg) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		sysvar := bone.GetValue(r, "sysvar")
+		if sysvar == "" {
+			reqres.RespondJSON(w, reqres.NewAPIError("sysvar parameter required", http.StatusBadRequest))
+			return
+		}
+
+		node, err := ws.Node(cf.NodeAddress)
+		if err != nil {
+			reqres.RespondJSON(w, reqres.NewAPIError(fmt.Sprintf("Error getting node: %s", err), http.StatusInternalServerError))
+			return
+		}
+
+		pageIndex, pageSize, errMsg, err := getPagingParams(r)
+		if errMsg != "" {
+			reqres.RespondJSON(w, reqres.NewFromErr(errMsg, err, http.StatusBadRequest))
+			return
+		}
+
+		result, _, err := tool.SysvarHistory(node, sysvar, pageIndex, pageSize)
+		if err != nil {
+			reqres.RespondJSON(w, reqres.NewAPIError(fmt.Sprintf("Error fetching key history: %s", err), http.StatusInternalServerError))
+			return
+		}
+
+		reqres.RespondJSON(w, reqres.OKResponse(result))
 	}
 }
