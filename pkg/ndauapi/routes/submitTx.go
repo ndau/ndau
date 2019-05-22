41df46e57459f44e030e4f83b0fed1c853725469
Eric Schmidt <eric.schmidt@oneiro.io>
2019-05-22 12:58:41 -0700

    Return success on submit and prevalidate if tx is already indexed

--- pkg/ndauapi/routes/submitTx.go
+++ pkg/ndauapi/routes/submitTx.go
@@ -1,33 +1,30 @@
 package routes
 
 import (
-	"encoding/base64"
+	"fmt"
 	"net/http"
 
 	"github.com/go-zoo/bone"
+	metatx "github.com/oneiro-ndev/metanode/pkg/meta/transaction"
 	"github.com/oneiro-ndev/ndau/pkg/ndau"
 	"github.com/oneiro-ndev/ndau/pkg/ndauapi/cfg"
 	"github.com/oneiro-ndev/ndau/pkg/ndauapi/reqres"
 	"github.com/oneiro-ndev/ndau/pkg/ndauapi/ws"
 	"github.com/oneiro-ndev/ndau/pkg/tool"
-	ctypes "github.com/tendermint/tendermint/rpc/core/types"
 )
 
 // SubmitResult is returned by the submit endpoint after the tx has been processed
 // by a node. If the transaction was valid and posted to the blockchain,
-// a TxHash is returned and ResultCode is 0 (and http status will be 200).
+// a TxHash is returned (and http status will be 200).
 // If the included POST body is missing, incorrectly formatted, or cannot be
 // submitted to the blockchain as a transaction, this function will
 // return 400 as the http status and the SubmitResult return object will not be included.
 // If the transaction parses correctly but is determined by the blockchain to be invalid,
-// ResultCode will be nonempty and the Msg field will contain a textual error explanation.
-// If ResultCode is nonempty, the http status will be 4xx.
 // If there was some internal processing error not related to the validity of the
 // request or transaction, then http status will be 5xx.
 type SubmitResult struct {
-	TxHash     string `json:"hash"`
-	ResultCode string `json:"result_code,omitempty"`
-	Msg        string `json:"msg,omitempty"`
+	TxHash string `json:"hash"`
+	Msg    string `json:"msg,omitempty"`
 }
 
 // HandleSubmitTx generates a handler that implements the /tx/submit endpoint
@@ -49,28 +46,30 @@ func HandleSubmitTx(cf cfg.Cfg) http.HandlerFunc {
 			return
 		}
 
-		// and now commit it synchronously
-		cr, err := tool.SendCommit(node, tx)
+		txhash := metatx.Hash(tx)
+
+		// Check if the tx has already been indexed.
+		blockheight, _, err := searchTxHash(node, txhash)
 		if err != nil {
-			// chances are high that if this fails, it's the user's fault, so let's
-			// blame them, not ourselves
-			reqres.RespondJSON(w, reqres.NewFromErr("error from commit", err, http.StatusBadRequest, tool.ResultLog(cr)))
+			reqres.RespondJSON(w, reqres.NewAPIError(fmt.Sprintf("txhash search failed: %v", err), http.StatusInternalServerError))
 			return
 		}
 
-		txresult, ok := cr.(*ctypes.ResultBroadcastTxCommit)
-		if !ok {
-			reqres.RespondJSON(w, reqres.NewFromErr("error casting tx result", err, http.StatusInternalServerError))
-			return
-		}
-
-		result := SubmitResult{TxHash: base64.StdEncoding.EncodeToString(txresult.Hash)}
+		result := SubmitResult{TxHash: txhash}
 		code := http.StatusOK // if we ever do this without synchronous commit, change to StatusAccepted
 
-		if err != nil {
-			result.ResultCode = "transaction error"
-			result.Msg = err.Error()
-			code = http.StatusBadRequest
+		// If we've got the tx indexed, it must already be on the blockchain; succeed by default.
+		if blockheight > 0 {
+			result.Msg = "tx already committed"
+		} else {
+			// commit it synchronously
+			cr, err := tool.SendCommit(node, tx)
+			if err != nil {
+				// chances are high that if this fails, it's the user's fault, so let's
+				// blame them, not ourselves
+				reqres.RespondJSON(w, reqres.NewFromErr("error from commit", err, http.StatusBadRequest, tool.ResultLog(cr)))
+				return
+			}
 		}
 
 		reqres.RespondJSON(w, reqres.Response{Bd: result, Sts: code})
