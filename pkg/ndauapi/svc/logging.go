2617153bfd058073de3bba7d18990b0a11b59d1d
Eric Schmidt <eric.schmidt@oneiro.io>
2019-07-11 12:37:05 -0700

    Make ndauapi always log json format

--- pkg/ndauapi/svc/logging.go
+++ pkg/ndauapi/svc/logging.go
@@ -2,6 +2,7 @@ package svc
 
 import (
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -29,8 +30,24 @@ func (w *LogWriter) Write(b []byte) (int, error) {
 
 // LogMW wraps a regular handler and replaces the writer with some logging middleware.
 func LogMW(handler http.Handler) http.HandlerFunc {
-
 	logger := logrus.New()
+	logger.Out = os.Stdout
+
+	// Procmon assumes json output from ndauapi.
+	logger.Formatter = &logrus.JSONFormatter{}
+
+	switch os.Getenv("LOG_LEVEL") {
+	case "info", "":
+		logger.Level = logrus.InfoLevel
+	case "debug":
+		logger.Level = logrus.DebugLevel
+	case "warn", "warning":
+		logger.Level = logrus.WarnLevel
+	case "err", "error":
+		logger.Level = logrus.ErrorLevel
+	default:
+		logger.Level = logrus.InfoLevel
+	}
 
 	return func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
