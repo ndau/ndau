1fe5f3f7619471731d29ab137b1c36b205e6e5db
Kent Quirk <kentquirk@gmail.com>
2019-08-14 14:27:03 -0400

    Remove MinNodeRegistrationStake system variable
    This variable was being applied to all stake and it definitely shouldn't be applied there, and stake rules should control minimums, so the sysvar isn't needed.

--- pkg/ndau/tx_stake.go
+++ pkg/ndau/tx_stake.go
@@ -6,8 +6,6 @@ import (
 	"github.com/oneiro-ndev/ndau/pkg/ndau/backing"
 	"github.com/oneiro-ndev/ndaumath/pkg/address"
 	"github.com/oneiro-ndev/ndaumath/pkg/signature"
-	math "github.com/oneiro-ndev/ndaumath/pkg/types"
-	sv "github.com/oneiro-ndev/system_vars/pkg/system_vars"
 	"github.com/pkg/errors"
 )
 
@@ -42,28 +40,18 @@ func (tx *Stake) Validate(appI interface{}) error {
 		}
 	}
 
-	var minStake math.Ndau
-	err = app.System(sv.MinNodeRegistrationStakeName, &minStake)
-	if err != nil {
-		return errors.Wrap(err, "fetching MinStake system variable")
-	}
-
-	if tx.Qty < minStake {
-		return fmt.Errorf("cannot stake %s ndau: must stake at least MinStake (%s)", tx.Qty, minStake)
-	}
-
 	txFee, err := app.calculateTxFee(tx)
 	if err != nil {
 		return errors.Wrap(err, "calculating tx fee")
 	}
 
-	requiredBalance, err := minStake.Add(txFee)
+	requiredBalance, err := tx.Qty.Add(txFee)
 	if err != nil {
 		return errors.Wrap(err, "calculating required balance")
 	}
 
 	if target.Balance.Compare(requiredBalance) < 0 {
-		return fmt.Errorf("target has insufficient balance: have %s ndau, need %s", target.Balance, minStake)
+		return fmt.Errorf("target has insufficient balance: have %s ndau, need %s", target.Balance, requiredBalance)
 	}
 
 	_, hasNode := app.getAccount(tx.StakeTo)
