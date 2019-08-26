4bd07bbda4e55ebed92435e1e521ebf9fe587083
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-08-26 17:03:02 +0200

    implement metadata API functions

--- pkg/api_sdk/metadata.go
+++ pkg/api_sdk/metadata.go
@@ -1,9 +1,48 @@
 package sdk
 
-// TODO: implement these
+import (
+	"github.com/oneiro-ndev/ndau/pkg/ndauapi/routes"
+	"github.com/pkg/errors"
+	rpctypes "github.com/tendermint/tendermint/rpc/core/types"
+)
 
-// func GetSIB(node *Client) (sib query.SIBResponse, resp *rpctypes.ResultABCIQuery, err error)
-// func GetSummary(node *Client) (*query.Summary, *rpctypes.ResultABCIQuery, error)
-// func Info(node client.Status*Client) (*rpctypes.ResultStatus, error)
-// func Nodes(node *client.HTTP) chan NodeResponse
-// func Version(node *Client) (string, *rpctypes.ResultABCIQuery, error)
+// Info gets the node's current status
+func (c *Client) Info() (status *rpctypes.ResultStatus, err error) {
+	err = c.get(status, c.URL("node/status"))
+	err = errors.Wrap(err, "fetching node status from API")
+	return
+}
+
+// Info gets the node's current status
+func Info(node *Client) (status *rpctypes.ResultStatus, err error) {
+	return node.Info()
+}
+
+// PriceInfo returns current price data for key parameters
+func (c *Client) PriceInfo() (info *routes.PriceInfo, err error) {
+	err = c.get(info, c.URL("price/current"))
+	err = errors.Wrap(err, "fetching price info from API")
+	return
+}
+
+// GetSIB exists for compatibility; it delegates to node.PriceInfo()
+func GetSIB(node *Client) (*routes.PriceInfo, error) {
+	return node.PriceInfo()
+}
+
+// GetSummary exists for compatibility; it delegates to node.PriceInfo()
+func GetSummary(node *Client) (*routes.PriceInfo, error) {
+	return node.PriceInfo()
+}
+
+// Version delivers version information
+func (c *Client) Version() (version *routes.VersionResult, err error) {
+	err = c.get(version, c.URL("version"))
+	err = errors.Wrap(err, "getting version from API")
+	return
+}
+
+// Version delivers version information
+func Version(node *Client) (*routes.VersionResult, error) {
+	return node.Version()
+}
