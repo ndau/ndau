57919a91555bf371c16772db73be0594f4002814
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-08-26 16:46:30 +0200

    implement sysvar functions

--- pkg/api_sdk/sysvar.go
+++ pkg/api_sdk/sysvar.go
@@ -1,8 +1,69 @@
 package sdk
 
-// TODO: implement these:
+import (
+	"strings"
 
-// func Sysvar(node *Client, name string, example msgp.Unmarshaler) error
-// func SysvarHistory(node *Client, name string, after uint64, limit int) (*query.SysvarHistoryResponse, *rpctypes.ResultABCIQuery, error)
-// func Sysvars(node *Client, vars ...string) (map[string][]byte, *rpctypes.ResultABCIQuery, error)
-// func SysvarsAsJSON(svs map[string][]byte) (jsvs map[string]interface{}, err error)
+	"github.com/oneiro-ndev/ndau/pkg/query"
+	"github.com/pkg/errors"
+	"github.com/tinylib/msgp/msgp"
+)
+
+// Sysvars gets the list of requested system variables, marshaled
+func (c *Client) Sysvars(vars ...string) (svs map[string][]byte, err error) {
+	var url string
+	if len(vars) == 0 {
+		url = c.URL("system/all")
+	} else {
+		url = c.URL("system/get/%s", strings.Join(vars, ","))
+	}
+	err = c.get(svs, url)
+	err = errors.Wrap(err, "getting sysvars")
+	return
+}
+
+// Sysvars gets the list of requested system variables, marshaled
+func Sysvars(node *Client, vars ...string) (map[string][]byte, error) {
+	return node.Sysvars(vars...)
+}
+
+// Sysvar gets a single system variable given its name and an example of its type
+//
+// The example is populated with the appropriate data
+func (c *Client) Sysvar(name string, example msgp.Unmarshaler) error {
+	svs, err := c.Sysvars(name)
+	if err != nil {
+		return errors.Wrap(err, "getting system variable")
+	}
+	sv, ok := svs[name]
+	if !ok {
+		return errors.New("API did not return requested system variable")
+	}
+	_, err = example.UnmarshalMsg(sv)
+	return errors.Wrap(err, "unmarshaling")
+}
+
+// Sysvar gets a single system variable given its name and an example of its type
+//
+// The example is populated with the appropriate data
+func Sysvar(node *Client, name string, example msgp.Unmarshaler) error {
+	return node.Sysvar(name, example)
+}
+
+// SysvarHistory gets the value history of the given sysvar.
+//
+// Pass in 0,0 for the paging params to get the entire history.
+func (c *Client) SysvarHistory(name string, after uint64, limit int) (resp *query.SysvarHistoryResponse, err error) {
+	err = c.get(resp, c.URLP(
+		map[string]interface{}{"after": after, "limit": limit},
+		"system/history/%s", name,
+	))
+	err = errors.Wrap(err, "getting sysvar history")
+	return
+}
+
+// SysvarHistory gets the value history of the given sysvar.
+//
+// Pass in 0,0 for the paging params to get the entire history.
+func SysvarHistory(node *Client, name string, after uint64, limit int) (*query.SysvarHistoryResponse, error) {
+	return node.SysvarHistory(name, after, limit)
+}
