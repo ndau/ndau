eee8b5b41c3ba7e42a9bb03420968b6123077cc1
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-08-27 09:56:13 +0200

    fix malformed URLs and eliminate functions not appearing in API

--- pkg/api_sdk/search.go
+++
@@ -1,6 +0,0 @@
-package sdk
-
-// TODO: implement these
-
-// func GetSearchResults(node *Client, params string) (string, error)
-// func SearchDateRange(node *Client, first, last string) (uint64, uint64, error)
