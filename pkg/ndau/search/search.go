2183e29d25769fa6a9d4b6ac9bab3643aa953881
Eric Schmidt <eric.schmidt@oneiro.io>
2019-09-05 09:29:04 -0700

    Handle empty results better

--- pkg/ndau/search/search.go
+++ pkg/ndau/search/search.go
@@ -132,23 +132,18 @@ func (search *Client) SearchTxTypes(txHash string, txTypes []string, limit int) 
 	defer search.Client.Del(searchKey)
 
 	// Merge sort all the requested tx type results.
-	_, err := search.Client.ZUnionStore(searchKey, searchKeys)
-	if err != nil {
+	count, err := search.Client.ZUnionStore(searchKey, searchKeys)
+	if err != nil || count == 0 {
+		// This will return an empty list with no error if the union returned zero count.
 		return listValueData, err
 	}
 
 	// Get the rank of the starting tx hash.  If it's not in the list, we have a bad query.
 	// Use reverse rank since we want transactions in reverse chronological order.
 	var start int64
-	if txHash == StartTxHash {
+	if txHash == StartTxHash || txHash == "" {
 		// Start from the latest tx.
 		start = 0
-
-		// Exit early if there are no results.
-		count, err := search.Client.ZCount(searchKey)
-		if err != nil || count <= 0 {
-			return listValueData, err
-		}
 	} else {
 		start, err = search.Client.ZRevRank(searchKey, txHash)
 		if err != nil {
