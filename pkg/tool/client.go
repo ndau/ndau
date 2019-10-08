44b077a77626f82c9b8953222d119c7e7d201a4d
Kent Quirk <kentquirk@gmail.com>
2019-10-08 16:48:37 -0400

    Add copyright and license info

--- pkg/tool/client.go
+++ pkg/tool/client.go
@@ -1,5 +1,14 @@
 package tool
 
+// ----- ---- --- -- -
+// Copyright 2019 Oneiro NA, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License 2.0 (the "License").  You may not use
+// this file except in compliance with the License.  You can obtain a copy
+// in the file LICENSE in the source distribution or at
+// https://www.apache.org/licenses/LICENSE-2.0.txt
+// - -- --- ---- -----
+
 import "github.com/tendermint/tendermint/rpc/client"
 
 // Client sets up a client connection to a Tendermint node from its address
