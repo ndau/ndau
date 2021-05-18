b02eb508afe44ab8b84e10e0a1d09c1a116f2054
John Greenland <jsgjnr@yahoo.com>
2021-05-18 10:06:17 -0400

    add logging for goodness args

--- pkg/ndau/chain_builders.go
+++ pkg/ndau/chain_builders.go
@@ -519,6 +519,15 @@ func BuildVMForNodeGoodness(
 		return nil, err
 	}
 
+	logger := app.DecoratedLogger().WithFields(log.Fields{
+		"address":         addr,
+		"rts":             rts,
+		"votingHistory":   votingHistory,
+		"totalDelegation": totalDelegation,
+		"totalStake":      totalStake,
+	})
+	logger.Info("nodegoodness args")
+
 	// goodness functions all use the default handler
 	err = theVM.Init(0, rtsV, votingHistoryV, totalDelegationV, addrV, acctV, totalStakeV)
 	return theVM, err
